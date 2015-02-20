dad391bd072410d8d022d6c575d61e697e5525f9
Pavel Odvody <podvody@redhat.com>
2015-02-20 11:48:23 +0100

    this should make much more sense
    Signed-off-by: Pavel Odvody <podvody@redhat.com>

--- graph/pull.go
+++ graph/pull.go
@@ -101,8 +101,7 @@ func (s *TagStore) CmdPull(job *engine.Job) engine.Status {
 	if job.GetenvBool("pullid") {
 		log.Debugf("pulling v1 repository by image id %q", imageid)
 		repoData, err := r.GetRepositoryData(logName)
-		var is_downloaded bool
-		if is_downloaded, err = s.pullImage(r, job.Stdout, imageid, repoInfo.Index.Mirrors[0], repoData.Tokens, sf); err != nil {
+		if _, err = s.pullImage(r, job.Stdout, imageid, repoInfo.Index.Mirrors[0], repoData.Tokens, sf); err != nil {
 			return job.Error(err)
 		}
 	} else {
