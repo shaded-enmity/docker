6688991cd6c210270117eff821465f3614c63027
Pavel Odvody <podvody@redhat.com>
2015-02-24 14:33:54 +0100

    cleanup

--- graph/push.go
+++ graph/push.go
@@ -375,7 +375,7 @@ func (s *TagStore) pushV2Repository(r *registry.Session, eng *engine.Engine, out
 
 		// push the manifest
 		var digest string
-		if digest, err := r.PutV2ImageManifest(endpoint, repoInfo.RemoteName, tag, bytes.NewReader([]byte(manifestBytes)), auth); err != nil {
+		if digest, err = r.PutV2ImageManifest(endpoint, repoInfo.RemoteName, tag, bytes.NewReader([]byte(manifestBytes)), auth); err != nil {
 			return "", err
 		}
 
