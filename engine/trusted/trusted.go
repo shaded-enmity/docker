89b44c34e102daa8a513a0ff929b99c5cfbc81b7
Pavel Odvody <podvody@redhat.com>
2015-03-23 17:16:58 +0100

    Pseudocode -> Go

--- engine/trusted/trusted.go
+++ engine/trusted/trusted.go
@@ -1,35 +1,40 @@
-package trusted;
+package trusted
+
+import (
+	"fmt"
+	"os"
+)
 
 const (
-	VERSION = 0x00010001,
-	VERSION_STRING = "0.1.0.1", // <majore>.<minor>.<build>.<release>
-	PARAMETER_STRING = "trusted",
-	HEADER_UID  = "X-Trusted-Client-Id",
-	HEADER_EUID = "X-Trusted-Client-EId"
+	VERSION          = 0x00010001
+	VERSION_STRING   = "0.1.0.1" // <majore>.<minor>.<build>.<release>
+	PARAMETER_STRING = "trusted"
+	HEADER_UID       = "X-Trusted-Client-Id"
+	HEADER_EUID      = "X-Trusted-Client-EId"
 	// ----------------------------------------------------------------------
 	TL_INVALID = iota
 	TL_ENFORCING
 	TL_PERMISSIVE
-);
+)
 
-type TrustLevel int;
-type Headers map[string]string;
+type TrustLevel int
+type Headers map[string]string
 
 type HdrTuple struct {
 	header string
-	id     int*
-};
+	id     *int
+}
 
 var (
 	Level = TL_ENFORCING
-);
+)
 
 func GetTrustLevel() TrustLevel {
-	return Level;
+	return Level
 }
 
 func SetTrustLevel(level TrustLevel) void {
-	Level = level;
+	Level = level
 }
 
 // ----------------------------------------------------------------------
@@ -37,13 +42,13 @@ func SetTrustLevel(level TrustLevel) void {
 //
 //   Executed by a trusted client. Add uid/euid headers to headers passed
 //   in the `headers` argument.
-// 
+//
 func DecorateRequest(headers Headers) (Headers, error) {
-	uid := os.Getuid();
-	euid := os.Geteuid();
-	
-	for _, hdr := range []HdrTuple { {HEADER_UID, &uid}, {HEADER_EUID, &euid} } {
-		headers[hdr.header] = fmt.Sprintf("%d", *hdr.id);
+	uid := os.Getuid()
+	euid := os.Geteuid()
+
+	for _, hdr := range []HdrTuple{{HEADER_UID, &uid}, {HEADER_EUID, &euid}} {
+		headers[hdr.header] = fmt.Sprintf("%d", *hdr.id)
 	}
 
 	return headers, nil
@@ -57,33 +62,33 @@ func DecorateRequest(headers Headers) (Headers, error) {
 //
 func ExtractHeaders(headers Headers) (int, int, error) {
 	var (
-		uid  = -1,
+		uid  = -1
 		euid = -1
 	)
 
-	for _, hdr := range []HdrTuple { {HEADER_UID, &uid}, {HEADER_EUID, &euid} } {
+	for _, hdr := range []HdrTuple{{HEADER_UID, &uid}, {HEADER_EUID, &euid}} {
 		if uid, exists := headers[hdr.header]; exists {
 			*hdr.id = int(uid)
 		} else {
 			if GetTrustLevel() == TL_ENFORCING {
-				return (uid, euid, error.Newf("Header %q not found!", hdr.header))
+				return uid, euid, fmt.Errorf("Header %q not found!", hdr.header)
 			} else {
-				log.Logf("[trusted] Header %q not found!", )
+				log.Logf("[trusted] Header %q not found!")
 			}
 		}
 	}
 
-	return (uid, euid, nil)
+	return uid, euid, nil
 }
 
 // ----------------------------------------------------------------------
 //   LabelProcess
 //
-//   Creates a string with SELinux label command given `uid` and target 
+//   Creates a string with SELinux label command given `uid` and target
 //   `pid`.
 //
 func LabelProcess(uid, pid int) (string, error) {
-	
+
 }
 
 // ----------------------------------------------------------------------
