79cbaf0567c97bdd11efc7b28a7a480447156711
Pavel Odvody <podvody@redhat.com>
2015-04-02 16:02:52 +0200

    object model woes

--- api/client/utils.go
+++ api/client/utils.go
@@ -19,7 +19,6 @@ import (
 	"github.com/docker/docker/api"
 	"github.com/docker/docker/autogen/dockerversion"
 	"github.com/docker/docker/engine"
-	"github.com/docker/docker/engine/trusted"
 	"github.com/docker/docker/pkg/signal"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/docker/pkg/term"
