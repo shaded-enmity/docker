d5730a9d058cfa55e843efedb33b193aa4310cd7
Pavel Odvody <podvody@redhat.com>
2015-04-02 11:03:31 +0200

    reflected woes

--- pkg/listenbuffer/buffer.go
+++ pkg/listenbuffer/buffer.go
@@ -47,7 +47,7 @@ func (l *defaultListener) Accept() (net.Conn, error) {
 		default:
 			log.Printf("unexpected type %T", v)
 		case *net.UnixConn:
-			fdt := reflect.ValueOf(&conn).FieldByName("conn").Type()
+			fdt := reflect.ValueOf(&conn).Kind()
 			log.Printf("unix socket %s", fdt)
 		}
 		return conn, err
