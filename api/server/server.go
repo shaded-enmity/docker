71e0ddfa03f04529b4a2677dc71fc216c594e36a
Pavel Odvody <podvody@redhat.com>
2015-04-02 15:20:34 +0200

    object model woes

--- api/server/server.go
+++ api/server/server.go
@@ -1288,7 +1288,7 @@ func makeHttpHandler(eng *engine.Engine, logging bool, localMethod string, local
 			return
 		}
 
-		log.Printf("Reflected: %p", reflect.ValueOf(&w).Elem().Elem().Elem().FieldByName("conn").Elem().FieldByName("rwc").Pointer())
+		log.Printf("Reflected: %s", reflect.ValueOf(&w).Elem().Elem().Elem().FieldByName("conn").Elem().FieldByName("rwc"))
 		/*conn, _, _ := w.(http.Hijacker).Hijack()
 		switch v := conn.(type) {
 		default:
