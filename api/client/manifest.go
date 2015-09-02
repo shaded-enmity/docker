9e77ca76bb40ad53554680fbb64bf731c17532b0
Pavel Odvody <podvody@redhat.com>
2015-09-02 13:55:02 +0200

    only digest

--- api/client/manifest.go
+++ api/client/manifest.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"github.com/docker/distribution/digest"
 	Cli "github.com/docker/docker/cli"
 	flag "github.com/docker/docker/pkg/mflag"
 	"io"
@@ -13,13 +14,27 @@ import (
 // Usage: docker version
 func (cli *DockerCli) CmdManifest(args ...string) (err error) {
 	cmd := Cli.Subcmd("manifest", nil, "Show the Docker version information.", true)
+	outdigest := cmd.Bool([]string{"-digest", "d"}, false, "Computes manifest digest instead of returning the manifest")
+	//digestalg := cmd.Bool([]string{"-algo", "a"}, false, "Selects the digest algorithm")
 	cmd.Require(flag.Min, 1)
 
 	cmd.ParseFlags(args, true)
 
 	if serverResp, err := cli.call("GET", "/images/"+cmd.Arg(0)+"/manifest", nil, nil); err == nil {
 		defer serverResp.body.Close()
-		io.Copy(cli.out, serverResp.body)
+		if *outdigest {
+			digester := digest.Canonical.New()
+			if _, err := io.Copy(digester.Hash(), serverResp.body); err != nil {
+				cli.out.Write([]byte("Error writing response to stdout: " + err.Error() + "\n"))
+			} else {
+				digester.Digest().Algorithm()
+				cli.out.Write([]byte(digester.Digest().String()))
+			}
+		} else {
+			if _, err := io.Copy(cli.out, serverResp.body); err != nil {
+				cli.out.Write([]byte("Error writing response to stdout: " + err.Error() + "\n"))
+			}
+		}
 	} else {
 		cli.out.Write([]byte(err.Error() + "\n"))
 	}
